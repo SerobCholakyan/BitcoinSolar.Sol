fc19c20b18edef33ad601ad7f4d24603f03f2f18
SerobCholakyan <serobSC@yahoo.com>
2026-02-21 09:12:19 -0800

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,7 +1,8 @@
+// preload.js
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('minerAPI', {
   onBlockSolved: (callback) => {
-    ipcRenderer.on('miner:block-solved', () => callback());
+    ipcRenderer.on("miner:block-solved", (_, data) => callback(data));
   }
 });
