eaab54a52fd2c80226f30d58c15c92ebbf6872e3
SerobCholakyan <serobSC@yahoo.com>
2026-02-28 15:17:52 -0800

    Update SecurityBridge.sol

--- contracts/SecurityBridge.sol
+++ contracts/SecurityBridge.sol
@@ -1,4 +1,14 @@
-@@ -6,6 +6,9 @@ import "@openzeppelin/contracts/access/Ownable.sol";
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * @title SecurityBridge
+ * @dev Authorization layer for backend mining nodes.
+ * Only the owner can add or remove authorized nodes.
+ * BitcoinSolar uses this to restrict mining execution.
+ */
 abstract contract SecurityBridge is Ownable {
     mapping(address => bool) private authorizedNodes;
 
@@ -7,8 +17,12 @@ abstract contract SecurityBridge is Ownable {
 
     constructor() Ownable(msg.sender) {
         authorizedNodes[msg.sender] = true;
+        emit NodeAuthorized(msg.sender);
     }
-@@ -16,10 +19,17 @@ abstract contract SecurityBridge is Ownable {
+
+    modifier onlyAuthorized() {
+        require(authorizedNodes[msg.sender], "SECURITY_ERR: UNAUTHORIZED_NODE");
+        _;
     }
 
     function addAuthorizedNode(address node) external onlyOwner {
