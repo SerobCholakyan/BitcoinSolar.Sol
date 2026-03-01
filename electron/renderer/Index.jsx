5de204ea5c5cb6b9a562967bfa70378fcd36c8fa
SerobCholakyan <serobSC@yahoo.com>
2026-03-01 07:06:40 -0800

    Create Index.jsx

---
+++ electron/renderer/Index.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import BlsrMintPanel from "./components/BlsrMintPanel";
+
+const root = createRoot(document.getElementById("root"));
+root.render(<BlsrMintPanel />);
