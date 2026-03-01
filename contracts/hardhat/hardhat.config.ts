86792a193ef963399201de8b56720d5f922db30d
SerobCholakyan <serobSC@yahoo.com>
2026-02-28 23:46:46 -0800

    Update and rename hardhat.config.js to hardhat.config.ts

---
+++ contracts/hardhat/hardhat.config.ts
@@ -0,0 +1,12 @@
+const config: HardhatUserConfig = {
+  solidity: "0.8.20", // Matches your OpenZeppelin v5.x needs
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_URL || "",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+};
