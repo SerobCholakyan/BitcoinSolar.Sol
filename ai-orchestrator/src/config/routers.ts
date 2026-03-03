9409763bb7077085a40b15efd7f3640e7a1b412e
SerobCholakyan <serobSC@yahoo.com>
2026-03-03 10:01:00 -0800

    Create routers.ts

---
+++ ai-orchestrator/src/config/routers.ts
@@ -0,0 +1,9 @@
+export const routers = {
+  ethereum: {
+    uniswapV2: "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f",
+    uniswapV3: "0xE592427A0AEce92De3Edee1F18E0157C05861564"
+  },
+  polygon: {
+    quickswap: "0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff"
+  }
+};
