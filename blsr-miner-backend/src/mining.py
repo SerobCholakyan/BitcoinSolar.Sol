29cd223adb498e30abb35feeaa125531d621cdd5
SerobCholakyan <serobSC@yahoo.com>
2026-03-06 18:30:56 -0800

    Update mining.py
    Signed-off-by: SerobCholakyan <serobSC@yahoo.com>

--- blsr-miner-backend/src/mining.py
+++ blsr-miner-backend/src/mining.py
@@ -1,7 +1,8 @@
 import os
 from web3 import Web3
 
-RPC = os.getenv("BLSR_RPC_URL")
+RPC = os.getenv("RPC_BLSR_CHAIN=https://blsr-rpc.bitcoinsolar.org
+")
 PRIVATE_KEY = os.getenv("BLSR_PRIVATE_KEY")
 CONTRACT = os.getenv("BLSR_CONTRACT_ADDRESS")
 
